1046eb1027505a17ec752629518e66a0d13aea2b
David Linus Briemann <dlb@mailbox.org>
2021-01-03 14:46:51 +0100

    ?

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dbriemann/monkey
+
+go 1.13
--- main.go
+++ main.go
@@ -17,4 +17,5 @@ func main() {
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
+	fmt.Println("hi")
 }
